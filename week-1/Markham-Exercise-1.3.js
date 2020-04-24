1762030491947f01b743eec0d2f00db200062b9f
Rochelle Markham <rmarkham@my365.bellevue.edu>
2020-04-23 19:29:54 -0500

    Exercise 1.3

--- week-1/Markham-Exercise-1.3.js
+++ week-1/Markham-Exercise-1.3.js
@@ -7,12 +7,17 @@
 ;=====================================
 */
 
+//sets up variable requiring a url as a parameter
 var url = require("url");
 
+//creates variable to parse the URL
 var parsedURL = url.parse("http://www.iamawesome.com/profile?Rochelle_Markham");
 
+//prints the protocol section of the URL
 console.log(parsedURL.protocol);
 
+//prints the host section of the URL
 console.log(parsedURL.host);
 
+//prints the query section of the URL
 console.log(parsedURL.query);
