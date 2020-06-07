6bf8c9c7640fa999bf0c1e6dc4bd9cebd52a3957
Rochelle Markham <rmarkham@my365.bellevue.edu>
2020-06-07 18:57:36 -0500

    Exercise 7.2

---
+++ week-7/tdd-example/test/Markham-Exercise-7.2.js
@@ -0,0 +1,20 @@
+/*
+========================================
+; Title: Exercise 7.2
+; Author: Professor Krasso
+; Date: 7 June 2020
+; Modified By: Rochelle Markham
+; Descripton: TDD in Action
+========================================
+*/
+
+//requires the assert module
+var assert = require("assert");
+
+//test for function that turns a string into an array
+describe("String#split", function(){
+    it("should return an array of fruits", function(){
+        assert(Array.isArray('Apple, Orange, Mango'.split(',')));
+    });
+});
+
---
+++ week-7/tdd-example/test/fruits.js
@@ -0,0 +1,15 @@
+/*
+========================================
+; Title: Exercise 7.2
+; Author: Professor Krasso
+; Date: 7 June 2020
+; Modified By: Rochelle Markham
+; Description: TDD in Action
+========================================
+*/
+
+function getFruits(str){
+    return str.split(',');
+};
+
+module.exports=getFruits;
