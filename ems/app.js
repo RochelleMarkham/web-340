80c9b339e83cf92eb1577beba6fbb9e960b85361
Rochelle Markham <rmarkham@my365.bellevue.edu>
2020-06-12 18:14:05 -0500

    Exercise 8.2

--- ems/app.js
+++ ems/app.js
@@ -15,62 +15,60 @@ var http = require("http");
 var path = require("path");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var helmet = require("helmet");
 
-//lets express know where to find the employee model
-var Employee = require("./models/employee")
-
-//mongoDB connection
-var mongoDB="mongodb+srv://RochelleMarkham:Camilla2008@buwebdev-cluster-1-yqegs.mongodb.net/test"
-mongoose.connect(mongoDB, {
-    useMongoClient: true
-});
-
-mongoose.Promise = global.Promise;
-
-//sets the mongoose connection to a variable
-var db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "MongoDB connection error: "));
-
-db.once("open", function(){
-    console.log("Application connected to MongoDB instance");
-});
-
-//sets the express application to a variable
+//initializes express
 var app = express();
-
-//lets express know the views are in a file named views
-app.set("views", path.resolve(__dirname, "views"));
 
 //lets express know where to find public/css files
 app.use(express.static(__dirname + '/public'));
 
+//uses the short version of the morgan logger
+app.use(logger("short")); 
+
+//uses the helmet cross site scripting filter
+app.use(helmet.xssFilter());
+
 //lets express know to use the ejs view engine
 app.set("view engine", "ejs");
 
-//uses the short version of the morgan logger
-app.use(logger("short")); 
+//lets express know the views are in a file named views
+app.set("views", path.resolve(__dirname, "views"));
 
 //responds with the homepage when the site is accessed
 app.get("/", function(request, response){
     response.render("index", {
-        title: "Home page"
+        title: "Home page",
     });
 });
 
-app.get("/new", function(request, response){
-    response.render("/new", {
-        title: "New employee"
-    });
-});
+//lets express know where to find the employee model
+var Employee = require("./models/employee")
 
 var employee = new Employee({
     firstName: "Rochelle",
     lastName: "Markham"
 });
 
+//mongoDB connection
+var mongoDB="mongodb+srv://RochelleMarkham:Camilla2008@buwebdev-cluster-1-yqegs.mongodb.net/test"
+mongoose.connect(mongoDB, {
+    useMongoClient: true
+});
+mongoose.Promise = global.Promise;
+
+//sets the mongoose connection to a variable
+var db = mongoose.connection;
+
+//prints error message if mongoose connection error
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+
+//prints message once mongoose is connected
+db.once("open", function(){
+    console.log("Application connected to MongoDB instance");
+});
+
 //creates a local server on port 8080 and prints message
 http.createServer(app).listen(8080, function(){
     console.log("Application started on port 8080!");
 });
-
