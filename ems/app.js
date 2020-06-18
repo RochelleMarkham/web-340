5009d9a07101859a260bb922d80dcdfe56392646
Rochelle Markham <rmarkham@my365.bellevue.edu>
2020-06-17 20:10:44 -0500

    EMS

--- ems/app.js
+++ ems/app.js
@@ -9,63 +9,121 @@
 */
 
 //requires the use of express, http, the morgan logger,
-//mongoose, and the use of file paths
+//mongoose, csrf, helmet, body parser, cookie parser 
+//and the use of file paths
 var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
-var mongoose = require("mongoose");
 var helmet = require("helmet");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var csrf = require("csurf");
+var mongoose = require("mongoose");
+const Employee = require("./models/employee");
 
-//initializes express
+//mongoDB connection
+var mongoDB="mongodb+srv://RochelleMarkham:Camilla2008@buwebdev-cluster-1-yqegs.mongodb.net/test";
+  mongoose.connect(mongoDB, {
+    promiseLibrary: require("bluebird"),
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  }).then(() => {
+    console.log("Application connected to MongoDB instance");
+  }).catch(err => {
+    console.log(`MongoDB Error: ${err.message}`);
+  });
+
+//set up csrf protection
+var csrfProtection = csrf({cookie:true});
+
+//initializes express application
 var app = express();
-
-//lets express know where to find public/css files
-app.use(express.static(__dirname + '/public'));
 
 //uses the short version of the morgan logger
 app.use(logger("short")); 
-
+//uses the extended body parser
+app.use(
+  bodyParser.urlencoded({
+      extended: true
+  })
+);
 //uses the helmet cross site scripting filter
 app.use(helmet.xssFilter());
-
-//lets express know to use the ejs view engine
-app.set("view engine", "ejs");
+//uses cookie parser
+app.use(cookieParser());
+//uses csurf Protection
+app.use(csrfProtection);
+//sets up use of csrf tokens
+app.use(function(request, response, next){
+  var token = request.csrfToken();
+  response.cookie('XSRF-TOKEN', token);
+  response.locals.csrfToken = token;
+  next();
+});
+//lets express know where to find public/css files
+app.use(express.static(__dirname + "/public"));
 
 //lets express know the views are in a file named views
 app.set("views", path.resolve(__dirname, "views"));
+//lets express know to use the ejs view engine
+app.set("view engine", "ejs");
 
-//responds with the homepage when the site is accessed
+//routing - responds with the homepage when the site 
+//is accessed
 app.get("/", function(request, response){
-    response.render("index", {
-        title: "Home page",
+      response.render("index", {
+        title: "Home Page"
+  });
+});
+
+//redirects to the new employee entry page
+app.get("/new", function(request, response){
+  response.render("new", {
+    title: "New Employee Entry",
+    message: "Enter New Employee"
+  });
+});
+
+//redirects to the list page 
+app.get("/list", function(request, response){
+  Employee.find({}, function(error, employees){
+    if (error) throw error;
+    response.render("list", {
+      title: "Current Employees",
+      employees: employees
     });
+  });
 });
 
-//lets express know where to find the employee model
-var Employee = require("./models/employee")
-
-var employee = new Employee({
-    firstName: "Rochelle",
-    lastName: "Markham"
-});
-
-//mongoDB connection
-var mongoDB="mongodb+srv://RochelleMarkham:Camilla2008@buwebdev-cluster-1-yqegs.mongodb.net/test"
-mongoose.connect(mongoDB, {
-    useMongoClient: true
-});
-mongoose.Promise = global.Promise;
-
-//sets the mongoose connection to a variable
-var db = mongoose.connection;
-
-//prints error message if mongoose connection error
-db.on("error", console.error.bind(console, "MongoDB connection error: "));
-
-//prints message once mongoose is connected
-db.once("open", function(){
-    console.log("Application connected to MongoDB instance");
+//redirects the page after form submission
+app.post("/process", function(request, response){
+    if(!request.body.txtFirstName){
+      response.status(400).send("Entries must have a name");
+      return;
+    }
+    if(!request.body.txtLastName){
+      response.status(400).send("Entries must have a name");
+      return;
+    } 
+    //get request form data 
+    var empName = request.body.txtFirstName + " " + request.body.txtLastName;
+    console.log(empName)
+    //create an employee model
+    var employee = new Employee({
+      name: empName
+    });
+    //save
+    employee.save(function(err){
+      if (err){
+        console.log(err);
+        throw err;
+      } else {
+        console.log(empName + " saved successfully!");
+        response.redirect("/");
+      }
+    });
 });
 
 //creates a local server on port 8080 and prints message
